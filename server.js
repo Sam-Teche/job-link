4b6c0a24bf3cb14c24c96f07d5330549600ced06
Sam-Teche <ogunrindesam@gmail.com>
2025-11-01 13:44:17 +0100

    i added comment the unnecessary frontend

--- server.js
+++ server.js
@@ -9,12 +9,13 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:5500",
+      "http://lancemployment.netlify.app/",
       "http://127.0.0.1:5500",
       "http://127.0.0.1:5500/frontend",
       "http://127.0.0.1:5500/frontend2",
       "https://joblinkadmin.netlify.app/",
-      "https://joblinkadmin.netlify.app", // Add your production frontend URL
+      "https://joblinkadmin.netlify.app",
+      "https://lancemployment.netlify.app" // Add your production frontend URL
     ],
     credentials: true,
     methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
